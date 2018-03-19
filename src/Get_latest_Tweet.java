ea8410adfe80923a663d5c2e8e4b6989fe13f5d0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-19 16:50:28 -0700

    i DONT KNOW WHAT HALF OF THIS IS SO YEAH ALSO PROGRESS WAS DONE ON TWEETER.

--- src/Get_latest_Tweet.java
+++ src/Get_latest_Tweet.java
@@ -7,6 +7,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
 public class Get_latest_Tweet implements ActionListener{
 JFrame frame;
 JPanel panel;
@@ -21,7 +27,7 @@ public static void main(String[] args) {
 		panel=new JPanel();
 		button1=new JButton();
 		button1.setText("TWEETS");
-		text=new JTextField();
+		text=new JTextField(50);
 		frame.add(panel);
 		panel.add(button1);
 		panel.add(text);
@@ -34,9 +40,39 @@ public static void main(String[] args) {
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
-	
+if(e.getSource()==button1) {
+	String input=text.getText();
+	String output= getLatestTweet(input);
+	System.out.println(output);
+}
+}
+
+
+
+private String getLatestTweet(String searchingFor) {
+
+    Twitter twitter = new TwitterFactory().getInstance();
+    AccessToken accessToken = new AccessToken(
+            "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+            "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+            "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+    twitter.setOAuthAccessToken(accessToken);
+
+    Query query = new Query(searchingFor);
+    try {
+        QueryResult result = twitter.search(query);
+        return result.getTweets().get(0).getText();
+    } catch (Exception e) {
+        System.err.print(e.getMessage());
+        return "What the heck is that?";
+    }
 }
 
 
 
 }
+
+
+
+
