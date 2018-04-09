cf7ae8081be322c8be0c9375c79224d19d4dd008
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-09 16:51:42 -0700

    YOLO

--- src/Typing_Tutor.java
+++ src/Typing_Tutor.java
@@ -1,11 +1,13 @@
 import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Typing_Tutor {
+public class Typing_Tutor implements KeyListener {
  JFrame frame;
 JPanel panel;
 JLabel label;
@@ -27,11 +29,33 @@ public static void main(String[] args) {
 	label.setText(letter+" ");
 	frame.add(panel);
 	panel.add(label);
+	frame.addKeyListener(this);
 	frame.pack();
-}
+	}
 	 char generateRandomLetter() {
 	    Random r = new Random();
 	    return (char) (r.nextInt(26) + 'a');
 	}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	if(arg0.getkeychar){
+		
+		
+		
+	}
 }
-
+}
