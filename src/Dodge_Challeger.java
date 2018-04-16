73c090d2c81c4bf0c07eb6df70ce6338ec0cf953
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-16 16:52:14 -0700

    OIDJjdnfjsndfjnsd

--- src/Dodge_Challeger.java
+++ src/Dodge_Challeger.java
@@ -1,4 +1,19 @@
 
 public class Dodge_Challeger {
-
+String color;
+int year;
+String mods;
+public static void main(String[] args) {
+	Dodge_Challeger c1 = new Dodge_Challeger();
+}	
+	public void setColor(String color) {
+		this.color+=color;
 }
+	public void setYear(int year) {
+	this.year+=year;	
+	}
+	public void mod(String mods) {
+		this.mods+=mods;
+	}
+}
+
