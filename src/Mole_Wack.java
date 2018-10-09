13544b81097b41e2c265fa8f44ccfd2a8a3e319c
308824 <308824@0338S-S-R0B6H0J.sdcs.local>
2018-10-09 14:23:42 -0700

    Almost done with Wacka mole

--- src/Mole_Wack.java
+++ src/Mole_Wack.java
@@ -1,46 +1,114 @@
+
+
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Mole_Wack implements ActionListener {
-	JFrame frame;
-	JPanel panel;
-	JButton Button;
-	JButton Button1;
-public static void main(String[] args) {
-	new Mole_Wack().createUI();
-	new Mole_Wack().drawButtons();
-}
-void drawButtons() {
-	Random randy = new Random();
-	randy.nextInt();
-	for (int i = 0; i < randy; i++) {
+	int Yo=0;
+	int of=0;
+	Date dab= new Date();
+	JFrame frame= new JFrame();
+	JPanel panel= new JPanel();
+	JButton button2= new JButton("MOLE!");
+	public static void main(String[] args) {
+new Mole_Wack().createUI();
+Mole_Wack mole= new Mole_Wack();
 		
 	}
+
+	public void createUI()
+	{
+		frame= new JFrame();
+		panel= new JPanel();
 	
-}
- void createUI() {
-	frame = new JFrame();
-	panel = new JPanel();
-	Button = new JButton();
-	Button1 = new JButton();
-	frame.add(panel);
-	panel.add(Button);
-	panel.add(Button1);
-	Button.addActionListener(this);
-	Button1.addActionListener(this);
-	Treat.setText("Treat");
-	Trick.setText("Trick");
-	frame.setVisible(true);
-	frame.pack();
-}
-@Override
-public void actionPerformed(ActionEvent arg0) {
-	// TODO Auto-generated method stub
+		Random randy= new Random();
+		int Randy= randy.nextInt(20);
+		int Randum= randy.nextInt(Randy);
 	
-}
-}
+		
+		for(int i=0; i<Randy; i+=1) {
+			
+		
+			if(i==Randum) {
+				button2= new JButton("MOLE!");
+				button2.addActionListener(this);
+				panel.add(button2);
+			}
+			else{
+				JButton button1= new JButton("nothing");
+				panel.add(button1);
+				
+			}
+			
+		}
+		
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(300, 300);
+	
+		
+		
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	
+	    if( e.getSource()==button2) {
+	    	Yo++;
+	    	playSound("bu.wav");
+	    	if(Yo>=10) {
+	    		
+	    		of++;
+	    		endGame(dab,of);
+	    		
+	    	}
+	    	else {
+		//JOptionPane.showMessageDialog(null, "u killed a mole, JEEEZZUS");
+		frame.removeAll();
+		frame.dispose();
+		
+	
+		createUI();
+	    	}
+		
+		}
+		else {
+			speak("Yeeer");
+		}
+		}
+	private void endGame(Date timeAtStart, int molesWhacked) {
+	     Date timeAtEnd = new Date();
+	     JOptionPane.showMessageDialog(null, "Your whack rate is "
+	          + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	          + " moles per second.");
+	}
+	private void playSound(String fileName) {
+	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+	     sound.play();
+	}
+	void speak(String words) {
+	     try {
+	          Runtime.getRuntime().exec("say " + words).waitFor();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+	}
